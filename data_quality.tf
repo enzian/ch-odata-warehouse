c5dc3dd1c3770aabf71288916cb00d7c3e2f4239
Michael (Enzian) Ingold <github@michaelingold.ch>
2026-05-27 14:35:55 +0200

    removed all legacy CR jobs and scheduler triggers

--- data_quality.tf
+++ data_quality.tf
@@ -12,6 +12,24 @@ EOT
 
   view {
     query = file("${path.root}/meteo/data_quality/freshness.sql")
-    use_legacy_sql = false    
+    use_legacy_sql = false
+  }
+}
+
+resource "google_bigquery_table" "q_measure_cnt_over_time" {
+  dataset_id = google_bigquery_dataset.ch_meteo.dataset_id
+  table_id   = "q_measure_cnt_over_time"
+
+  description = <<-EOT
+  
+EOT
+
+  labels = {
+    env = "dev"
+  }
+
+  view {
+    query = file("${path.root}/meteo/data_quality/measures_cnt_over_time.sql")
+    use_legacy_sql = false
   }
 }
